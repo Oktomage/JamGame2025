521f608d43f90871183c33ab7d188a532ac41d18
patolino726 <pedrodepaularocha1234@gmail.com>
2025-04-26 16:59:35 -0300

    Spawn

--- JamGame/Assets/Scripts/Enemys/SpawnEnemy.cs
+++ JamGame/Assets/Scripts/Enemys/SpawnEnemy.cs
@@ -5,9 +5,38 @@ using UnityEngine;
 public class SpawnEnemy : MonoBehaviour
 {
 
-    void Test()
+    public List<GameObject> Level1_Formiga;
+    public float Cooldown_;
+    public float Value_Cooldown;
+
+
+
+    private void Update()
     {
-        Debug.Log("Deu certo");
+        Cooldow();
+    }
+    private void Start()
+    {
+       
+           }
+
+    void Cooldow()
+    {
+        if (Cooldown_ > 0)
+        {
+            Cooldown_ -= Time.deltaTime;
+        }
+
+        else if (Cooldown_ <= 0 )
+        {
+
+            Instantiate(Level1_Formiga[Random.Range(-0, Level1_Formiga.Count)], new Vector3(-9, Random.Range(12f, -12f), 0), Quaternion.identity);
+            Instantiate(Level1_Formiga[Random.Range(0, Level1_Formiga.Count)], new Vector3(9, Random.Range(-12f, 12f), 0), Quaternion.identity);
+            Cooldown_ = Value_Cooldown;
+        }
+
+
+
     }
 
 
