f22b30d84103735dc7d6b483274cc01a7bb40680
patolino726 <pedrodepaularocha1234@gmail.com>
2025-04-26 14:07:45 -0300

    Formiga
    dwa

---
+++ JamGame/Assets/Scripts/Enemys/SpawnEnemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+
+    void Test()
+    {
+        Debug.Log("Deu certo");
+    }
+
+
+}
