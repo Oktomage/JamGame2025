368a2f3901d5b07b4311dddb98ae44833d5a619e
patolino726 <pedrodepaularocha1234@gmail.com>
2025-04-26 13:40:56 -0300

    Formiga
    as

--- JamGame/Assets/Scripts/Formiga/FormigaBehaviour.cs
+++ JamGame/Assets/Scripts/Formiga/FormigaBehaviour.cs
@@ -6,48 +6,69 @@ public class FormigaBehaviour : MonoBehaviour
 
 {
     public GameObject atackWarning;
-    EntityStats entity;
+    public GameObject Arvore;
+    public float Cooldown_;
+    public float Cooldown_value;
 
-
-
+    private EntityStats entity;
+    private ArvoreStatsHP arvoreStats;
 
     private void Start()
     {
-        entity = gameObject.GetComponent<EntityStats>();
+        entity = GetComponent<EntityStats>();
+        arvoreStats = Arvore.GetComponent<ArvoreStatsHP>();
     }
 
+    private void Update()
+    {
+        Cooldown();
+    }
 
+    void Cooldown()
+    {
+        float Dist = Vector2.Distance(transform.position, Arvore.transform.position);
 
+        if (Cooldown_ > 0)
+        {
+            Cooldown_ -= Time.deltaTime;
+        }
+        else if (Dist <= 1.3f)
+        {
+            arvoreStats.Hp_Arvore -= entity.Atack_Damage;
+            Cooldown_ = Cooldown_value;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-    
+        if (collision.gameObject.CompareTag("Arvore"))
         {
-            if (collision.CompareTag("Arvore"))
-            {
-                ArvoreStatsHP arvore = collision.GetComponent<ArvoreStatsHP>();
-
-                if (arvore != null)
-                {
-                    arvore.Hp_Arvore -= entity.Atack_Damage;
-
-                    if (atackWarning != null)
-                        atackWarning.SetActive(true);
-                }
-            }
+            Arvore = collision.gameObject;
+            arvoreStats = Arvore.GetComponent<ArvoreStatsHP>();
+            atackWarning.SetActive(true);
         }
-
-     
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Arvore") && atackWarning != null)
+        if (collision.gameObject.CompareTag("Arvore"))
         {
             atackWarning.SetActive(false);
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
