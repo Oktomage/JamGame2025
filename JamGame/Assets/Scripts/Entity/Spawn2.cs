40f4a58ff37b8b0e54227c0aaef4582fa41a1955
patolino726 <pedrodepaularocha1234@gmail.com>
2025-04-27 22:34:50 -0300

    game over

--- JamGame/Assets/Scripts/Entity/Spawn2.cs
+++ JamGame/Assets/Scripts/Entity/Spawn2.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Spawn2 : MonoBehaviour
 {
     public List<GameObject> objetosParaSpawnar;
-    public int indiceDoObjeto = 0;
     public float intervaloDeSpawn = 2f;
     public float variacaoIntervalo = 1f;
     public Vector2 areaDeSpawnMin;
@@ -22,14 +21,16 @@ public class Spawn2 : MonoBehaviour
     {
         if (Time.time >= tempoProximoSpawn)
         {
-            SpawnarObjetoEmPosicaoAleatoria();
+            SpawnarObjetoAleatorio();
             CalcularProximoSpawn();
         }
     }
 
-    void SpawnarObjetoEmPosicaoAleatoria()
+    void SpawnarObjetoAleatorio()
     {
-        if (objetosParaSpawnar.Count == 0 || indiceDoObjeto >= objetosParaSpawnar.Count) return;
+        if (objetosParaSpawnar.Count == 0) return;
+
+        int indiceAleatorio = Random.Range(0, objetosParaSpawnar.Count);
 
         Vector3 posicaoAleatoria = new Vector3(
             Random.Range(areaDeSpawnMin.x, areaDeSpawnMax.x),
@@ -37,7 +38,7 @@ public class Spawn2 : MonoBehaviour
             0
         );
 
-        Instantiate(objetosParaSpawnar[indiceDoObjeto], posicaoAleatoria, Quaternion.identity);
+        Instantiate(objetosParaSpawnar[indiceAleatorio], posicaoAleatoria, Quaternion.identity);
     }
 
     void CalcularProximoSpawn()
@@ -46,3 +47,5 @@ public class Spawn2 : MonoBehaviour
         tempoProximoSpawn = Time.time + Mathf.Max(0.1f, intervaloAleatorio);
     }
 }
+
+
